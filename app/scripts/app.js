c9a818c896e85c8a082ab7e58a543c5150dbed89
gcosmic <cou.guillaume@gmail.com>
2015-02-04 23:29:42 -0500

    Added table layout and basic structure, modified stop-watch logic

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,9 +1,16 @@
 (function (document) {
   'use strict';
+  
+  window.pad = function(n, width, z) {
+	  z = z || '0';
+	  n = n + '';
+	  return n.length >= width ? n : new Array(width - n.length + 1).join(z) + n;
+	}
 
   document.addEventListener('polymer-ready', function () {
     // Perform some behaviour
-    console.log('Polymer is ready to rock!');
+    //console.log('Polymer is ready to rock!');
+	
   });
 
 // wrap document so it plays nice with other libraries
